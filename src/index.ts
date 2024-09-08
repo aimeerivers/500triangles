6e5a4e51024eb0013676806387531990bca1701a
aimee rivers <aimee@aimeerivers.com>
2024-09-08 12:19:05 +0200

    feat: reduce initial mutation to 10%

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ const numberOfTriangles = 500;
 // Genetic Algorithm Parameters
 const populationSize = 100;
 const generations = 200;
-const initialMutationRate = 0.2;
+const initialMutationRate = 0.1;
 const eliteSize = 10;
 
 let mutationRate = initialMutationRate;
